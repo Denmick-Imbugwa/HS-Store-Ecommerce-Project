a0364f63ac9c306f7f55a24e01738f3f08119b04
Jan Nelson <jannelson36@gmail.com>
2021-06-16 22:50:26 +0300

    master

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "ecommerce_db");
+		$this->con = new Mysqli("localhost", "root", "", "linelpoultry");
 		return $this->con;
 	}
 }
